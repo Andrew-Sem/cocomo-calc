0a967a9e5e79f8510744e3afe08abb4fb8d52649
Andrew-Sem <avvyayvayva@gmail.com>
2024-02-09 21:04:05 +0300

    chore: remove unused import

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,5 +1,4 @@
 import { BaseCard } from "@/components/base/card";
-import { Toaster } from "@/components/ui/toaster";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/")({
